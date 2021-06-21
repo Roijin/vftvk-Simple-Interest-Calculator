2f716f982e19ce47303eb06239df4954a2f5e2fe
Roijin <raihanbabang9@gmail.com>
2021-06-21 05:17:48 +0000

    All is done

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //get variables from html
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    //check if the principal is positive
+    if (principal<1){
+        alert("Enter a positive number")
+        document.getElementById("principal").focus();
+        return;
+    }
+    //return the results
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
     
 }
         
+//function to update the value
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML=rateval+"%";
+}
